b89ad48faef1f5d5cf553094c0663afb670ff428
Gina Liao <gina@ginaliao.com>
2016-01-04 11:52:18 +1300

    Add animation to footer icons

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,33 @@
-(function($) {
-  // Your handcrafted scripts go here!
+(function() {
 
-  var module = (function() {
-    var privateVar = true;
+  "use strict";
 
-    var doSomething = function() {
+  // Attaching to window object so the Grunticon callback will have access
+  window.icons = {};
 
-    };
+  icons.init = function() {
 
-    var doSomethingElse = function() {
+    // NOTE TO SELF: When optimising SVG, DO NOT convert path data; this will make Snap.svg animation go bonkers
+    // (when animating to original state, the path becomes contorted)
 
-    };
+    var icons = [
+      { name: 'twitter', colour: '#00ACED'},
+      { name: 'linkedin', colour: '#007BB6'},
+      { name: 'mail', colour: '#D64254'}
+    ];
 
-    return {
-      doSomething: doSomething,
-      doSomethingElse: doSomethingElse
-    };
-  }());
+    icons.forEach(function(icon, i) {
+      var s = Snap('#icon-' + icon.name);
+      s.hover(function() {
+        s.select('#bg-' + icon.name).stop()
+          .animate({ d: 'M36.2 7.8C28.3 0 20.5-4.9 7.8 7.8S0 28.3 7.8 36.2c7.9 7.9 15.7 12.7 28.4 0s7.8-20.5 0-28.4z' }, 1000, mina.bounce)
+          .animate( { fill: icon.colour }, 500, mina.linear);
+      }, function() {
+        s.select('#bg-' + icon.name).stop()
+        .animate({ d: 'M22,0C9.8,0,0,2.3,0,22c0,19.7,9.8,22,22,22 s22-2.3,22-22C44,2.3,34.2,0,22,0z', fill: '#3F4D5F' }, 500, mina.easein);
+      });
+    });
+    
+  };
 
-  var init = (function() {
-    var bindEvents = function() {
-      $('#thing').on('click', module.doSomething);
-    };
-
-    var start = function() {
-      bindEvents();
-    };
-
-    return {
-      bindEvents: bindEvents,
-      start: start
-    };
-  }());
-
-  init.start();
-}(jQuery));
+}());
