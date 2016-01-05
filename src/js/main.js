a6a41dcbbe08c7b65a8c40983dde40050d99dbf1
Gina Liao <gina@ginaliao.com>
2016-01-06 11:06:31 +1300

    Initialise Grunticon after dependencies have loaded

--- src/js/main.js
+++ src/js/main.js
@@ -2,33 +2,35 @@
 
   "use strict";
 
-  // Attaching to window object so the Grunticon callback will have access
-  window.icons = {};
+  var icons = (function() {
 
-  icons.init = function() {
+    var init = function() {
+      // NOTE TO SELF: When optimising SVG, DO NOT convert path data; this will make Snap.svg animation go bonkers
+      // (when animating to original state, the path becomes contorted)
 
-    // NOTE TO SELF: When optimising SVG, DO NOT convert path data; this will make Snap.svg animation go bonkers
-    // (when animating to original state, the path becomes contorted)
+      var socialIcons = [
+        { name: 'twitter', colour: '#00ACED'},
+        { name: 'linkedin', colour: '#007BB6'},
+        { name: 'mail', colour: '#D64254'}
+      ];
 
-    var icons = [
-      { name: 'twitter', colour: '#00ACED'},
-      { name: 'linkedin', colour: '#007BB6'},
-      { name: 'mail', colour: '#D64254'}
-    ];
-
-    icons.forEach(function(icon, i) {
-      var s = Snap('#icon-' + icon.name);
-      s.hover(function() {
-        s.select('#bg-' + icon.name).stop()
-          .animate({ d: 'M36.2 7.8C28.3 0 20.5-4.9 7.8 7.8S0 28.3 7.8 36.2c7.9 7.9 15.7 12.7 28.4 0s7.8-20.5 0-28.4z' }, 1000, mina.bounce)
-          .animate( { fill: icon.colour }, 500, mina.linear);
-      }, function() {
-        s.select('#bg-' + icon.name).stop()
-        .animate({ d: 'M22,0C9.8,0,0,2.3,0,22c0,19.7,9.8,22,22,22 s22-2.3,22-22C44,2.3,34.2,0,22,0z', fill: '#3F4D5F' }, 500, mina.easein);
+      $.each(socialIcons, function(i, icon) {
+        var s = Snap('#icon-' + icon.name);
+        s.hover(function() {
+          s.select('#bg-' + icon.name).stop()
+            .animate({ d: 'M36.2 7.8C28.3 0 20.5-4.9 7.8 7.8S0 28.3 7.8 36.2c7.9 7.9 15.7 12.7 28.4 0s7.8-20.5 0-28.4z' }, 1000, mina.bounce)
+            .animate( { fill: icon.colour }, 500, mina.linear);
+        }, function() {
+          s.select('#bg-' + icon.name).stop()
+          .animate({ d: 'M22,0C9.8,0,0,2.3,0,22c0,19.7,9.8,22,22,22 s22-2.3,22-22C44,2.3,34.2,0,22,0z', fill: '#3F4D5F' }, 500, mina.easein);
+        });
       });
-    });
-    
-  };
+    };
+
+    return {
+      init: init
+    };
+  }());
 
   var form = (function() {
 
@@ -124,6 +126,10 @@
     };
 
     var start = function() {
+      grunticon(["/img/icons/icons.data.svg.css", "/img/icons/icons.data.png.css", "/img/icons/icons.fallback.css"], function() {
+        grunticon.svgLoadedCallback(icons.init);
+      });
+
       bindEvents();
     };
 
