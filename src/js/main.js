1155621980ede3fd17e8339ec1e499d72e67b87e
Gina Liao <gina@ginaliao.com>
2016-01-06 23:12:16 +1300

    Initialise Snap on multiple instances of icons

--- src/js/main.js
+++ src/js/main.js
@@ -14,15 +14,17 @@
         { name: 'mail', colour: '#D64254'}
       ];
 
-      $.each(socialIcons, function(i, icon) {
-        var s = Snap('#icon-' + icon.name);
-        s.hover(function() {
-          s.select('#bg-' + icon.name).stop()
-            .animate({ d: 'M36.2 7.8C28.3 0 20.5-4.9 7.8 7.8S0 28.3 7.8 36.2c7.9 7.9 15.7 12.7 28.4 0s7.8-20.5 0-28.4z' }, 1000, mina.bounce)
-            .animate( { fill: icon.colour }, 500, mina.linear);
-        }, function() {
-          s.select('#bg-' + icon.name).stop()
-          .animate({ d: 'M22,0C9.8,0,0,2.3,0,22c0,19.7,9.8,22,22,22 s22-2.3,22-22C44,2.3,34.2,0,22,0z', fill: '#3F4D5F' }, 500, mina.easein);
+      $.each(socialIcons, function(i, socialIcon) {
+        $('.icon-' + socialIcon.name).each(function() {
+          var s = Snap(this);
+          s.hover(function() {
+            s.select('.bg-' + socialIcon.name).stop()
+              .animate({ d: 'M36.2 7.8C28.3 0 20.5-4.9 7.8 7.8S0 28.3 7.8 36.2c7.9 7.9 15.7 12.7 28.4 0s7.8-20.5 0-28.4z' }, 1000, mina.bounce)
+              .animate( { fill: socialIcon.colour }, 500, mina.linear);
+          }, function() {
+            s.select('.bg-' + socialIcon.name).stop()
+            .animate({ d: 'M22,0C9.8,0,0,2.3,0,22c0,19.7,9.8,22,22,22 s22-2.3,22-22C44,2.3,34.2,0,22,0z', fill: '#3F4D5F' }, 500, mina.easein);
+          });
         });
       });
     };
